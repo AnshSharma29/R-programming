cf3fbc39d63eadae8e6fe7208fbf10150162ff72
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2024-03-17 01:24:17 +0530

    Add files via upload

---
+++ Module 1 Coursera.R
@@ -0,0 +1,8 @@
+install.packages("palmerpenguins")
+library("palmerpenguins")
+summary(penguins)
+View(penguins)
+install.packages("tidyverse")
+library(tidyverse)
+library(ggplot2)
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g))+geom_point(aes(color=species))
