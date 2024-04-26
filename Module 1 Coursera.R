c361cd98852a5bc19114927c93aae7c22b06fffd
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2024-04-27 01:09:34 +0530

    Update Module 1 Coursera.R

--- Module 1 Coursera.R
+++ Module 1 Coursera.R
@@ -1,3 +1,4 @@
+
 install.packages("palmerpenguins")
 library("palmerpenguins")
 summary(penguins)
@@ -6,5 +7,4 @@ install.packages("tidyverse")
 library(tidyverse)
 library(ggplot2)
 ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g))+geom_point(aes(color=species))
-
 
